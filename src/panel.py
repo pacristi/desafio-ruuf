cdadec5ed1b5c4ea2ef0f5647f0aa18cc774be94
pacristi <piero.cristi@uc.cl>
2024-12-07 21:52:42 -0300

    style: remove ' from type

--- src/panel.py
+++ src/panel.py
@@ -3,10 +3,10 @@ from .rectangle import Rectangle
 
 class Panel(Rectangle):
 
-    def can_fit_from_position_vertically(self, position: int,  rectangle: 'Rectangle'):
+    def can_fit_from_position_vertically(self, position: int,  rectangle: Rectangle):
         return self.height + position <= rectangle.height
 
-    def can_fit_from_position_horizontally(self, position: int,  rectangle: 'Rectangle'):
+    def can_fit_from_position_horizontally(self, position: int,  rectangle: Rectangle):
         return self.width + position <= rectangle.width
 
     def flip(self) -> 'Panel':
